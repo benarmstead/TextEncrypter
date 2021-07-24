76bdd1494708363bd16ddf5b0116f3152e281dac
Ben Armstead <benarmstead@protonmail.com>
2021-07-24 17:38:00 +0100

    Remove license headers

--- encryption_code/encryption_menu.py
+++ encryption_code/encryption_menu.py
@@ -1,21 +1,3 @@
-'''
-TextEncrypter - Encrypts text before being sent
-Copyright (C) 2020  Ben Armstead
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
-
 import encryption_code.encryption as encryption
 import encryption_code.decryption as decryption
 import others.clear_s as clear_s
